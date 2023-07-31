6ea80ea1ddd6d72761c08f1d32baeac1d368ba71
Földvári Tamás <joker565656@gmail.com>
2023-07-31 12:01:33 +0200

    add first two models

---
+++ visa-application-website-API/visa-application-website-API/Models/Day.cs
@@ -0,0 +1,11 @@
+﻿namespace visa_application_website_API.Models
+{
+    public class Day
+    {
+        public int Id { get; private set; }
+        public DateTime Date { get; private set; }
+        public int userId { get; private set; }
+        public User User { get; private set; }
+
+    }
+}
---
+++ visa-application-website-API/visa-application-website-API/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+using System.Xml.Linq;
+
+namespace visa_application_website_API.Models
+{
+    public class User
+    {
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public ICollection<Day> Days { get; private set; }
+
+
+        /*protected User(int id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+        }*/
+    }
+
+}
